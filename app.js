fdbde36a15fdf1b8386f1cf7f1497f97ec451a71
user <user@Eno-ITs-MacBook-Pro.local>
2023-11-13 15:47:26 +0700

    can play, need to improve the win announcement

--- app.js
+++ app.js
@@ -27,17 +27,40 @@ const emojiList = [
 
 let shufEmojiList = shuffleArray(emojiList);
 
+let openedBoxes = [];
+
+function checkWin() {
+  const matchedBoxes = document.querySelectorAll('.boxMatch');
+  if (matchedBoxes.length === emojiList.length) {
+    alert('You won!');
+  }
+}
+
 for (let i = 0; i < shufEmojiList.length; i++) {
   let box = document.createElement('div');
   box.className = 'item';
   box.innerHTML = shufEmojiList[i];
 
   box.onclick = function () {
-    if (!this.classList.contains('boxOpen')) {
+    if (!this.classList.contains('boxOpen') && openedBoxes.length < 2) {
       this.classList.add('boxOpen');
-      setTimeout(() => {
-        this.classList.remove('boxOpen');
-      }, 1_500); // Thời gian mở hộp trước khi đóng trở lại (tính bằng mili giây)
+      openedBoxes.push(this);
+
+      if (openedBoxes.length === 2) {
+        if (openedBoxes[0].innerHTML === openedBoxes[1].innerHTML) {
+          openedBoxes[0].classList.add('boxMatch');
+          openedBoxes[1].classList.add('boxMatch');
+
+          checkWin();
+
+          openedBoxes = [];
+        } else {
+          setTimeout(() => {
+            openedBoxes.forEach(box => box.classList.remove('boxOpen'));
+            openedBoxes = [];
+          }, 500);
+        }
+      }
     }
   };
 
